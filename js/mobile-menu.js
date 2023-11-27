53e5ec401a39e6a33845d021e7196e4c9a1b79d5
Natika888 <krinata888@gmail.com>
2023-11-27 17:50:32 +0200

    add burger menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -5,33 +5,38 @@ const menuList = document.querySelector('.menu__list');
 
 // const toggleMenu = () => headerNav.classList.toggle('active');
 
-// menuBtnOpen.addEventListener('click', toggleMenu);
-// menuBtnClose.addEventListener('click', toggleMenu);
+function toggleMenu(e) {
+    headerNav.classList.toggle('active');
+    menuBtnClose.classList.toggle('visually-hidden');
+    menuBtnOpen.classList.toggle('visually-hidden');
+    document.body.classList.toggle('lock');
+}
+
+menuBtnOpen.addEventListener('click', toggleMenu);
+menuBtnClose.addEventListener('click', toggleMenu);
 
 function onMenuLinkClick(e) {
-    
-    if (menuBtnOpen.classList.contains('visually-hidden')) {
-        document.body.classList.remove('lock');
-        menuList.classList.remove('active');
-        menuBtnOpen.classList.remove('active');
-        menuBtnClose.classList.add('visually-hidden');
-    }
-    window.scrollTo( {
-        behavior: 'smooth'
+    document.body.classList.remove('lock');
+    headerNav.classList.remove('active');
+    menuBtnOpen.classList.remove('visually-hidden');
+    menuBtnClose.classList.add('visually-hidden');
+
+    window.scrollTo({
+        behavior: 'smooth',
     });
 }
 
-if (menuBtnOpen) {    
-    menuBtnOpen.addEventListener('click', function (e) {
-        document.body.classList.toggle('lock');
-        menuList.classList.toggle('active');
-        menuBtnOpen.classList.toggle('visually-hidden'); 
-        menuBtnClose.classList.toggle('visually-hidden');        
-    })
-}
+// if (menuBtnOpen) {
+//     menuBtnOpen.addEventListener('click', function (e) {
+//         // document.body.classList.toggle('lock');
+//         // menuList.classList.toggle('active');
+//         // menuBtnOpen.classList.toggle('visually-hidden');
+//         // menuBtnClose.classList.remove('visually-hidden');
+//     })
+// }
 
 const menuLinks = document.querySelectorAll('.menu__link');
 
 menuLinks.forEach(menuLink => {
     menuLink.addEventListener('click', onMenuLinkClick);
-})
+});
