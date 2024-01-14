de41ad955e6d9dca4ab0bb9152125790140899af
Natika888 <krinata888@gmail.com>
2024-01-14 15:24:05 +0200

    fix popap

--- js/modal.js
+++ js/modal.js
@@ -3,23 +3,30 @@ const modal = document.querySelector('.modal');
 const modalBtnOpen = document.querySelector('.btn');
 const modalBtnClose = document.querySelector('.close');
 const body = document.querySelector('body');
+const findFormBtn = document.querySelector('.find__form-btn');
 
 
-const toggleModal = () => {
-backdrop.classList.toggle('visually-hidden');
-body.classList.toggle('lock');
+const openModal = () => {
+    body.classList.add('lock');
+    backdrop.classList.remove('visually-hidden');
 }
 
-document.addEventListener('click', (e) => {
-    const click = e.composedPath().includes(modal);
-    const click2 = e.composedPath().includes(modalBtnOpen);
-    
-    if (!click && !click2) {
-        toggleModal();
+const closeModal = () => {
+    body.classList.remove('lock');
+    backdrop.classList.add('visually-hidden');
+}
+
+document.addEventListener('click', (e) => {    
+    const clickOnModal = e.composedPath().includes(modal);
+    const clickOnBtnOpen = e.composedPath().includes(modalBtnOpen);
+    const isBodyLocked = body.classList.contains('lock');
+
+    if (!clickOnModal&&!clickOnBtnOpen&&isBodyLocked){        
+        closeModal();
     } else {
         return;
     }
 });
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
 
+modalBtnOpen.addEventListener('click', openModal);
+modalBtnClose.addEventListener('click', closeModal);
